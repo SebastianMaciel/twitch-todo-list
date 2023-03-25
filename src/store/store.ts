82b1da283ecc364b355377e224f6803a6bfc7aeb
sebastianmaciel <sebastianmaciel@outlook.com>
2023-03-25 00:48:32 -0300

    Refactor and comments

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,7 @@
-// store.ts
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+import * as utils from './todoFunctions';
 import { Filter, PersistedTodoState, TodoState } from './types';
-import {
-  getTodos,
-  getCompletionResume,
-  addTodo,
-  removeTodo,
-  clearTodos,
-  editTodo,
-  toggleTodo,
-  setAllowRepeatedDescriptions,
-} from './todoFunctions';
-
-// Importa los mocks
 import { mockTodoList } from '../mocks/mocks';
 
 // Lee la variable de entorno
@@ -22,16 +10,20 @@ const useMocks = import.meta.env.VITE_USE_MOCKS === 'true';
 const useStore = create(
   persist<PersistedTodoState & TodoState>(
     (set, get) => ({
+      // Lista principal
       todos: useMocks ? mockTodoList : [],
+      // Configuraciones
       allowRepeatedDescriptions: false,
-      setAllowRepeatedDescriptions: (value: boolean) => setAllowRepeatedDescriptions(set, value),
-      getTodos: (filter: Filter) => getTodos(get, filter),
-      getCompletionResume: () => getCompletionResume(get),
-      addTodo: (todo) => addTodo(set, get, todo),
-      removeTodo: (id) => removeTodo(set, get, id),
-      clearTodos: () => clearTodos(set, get),
-      editTodo: (id, description) => editTodo(set, get, id, description),
-      toggleTodo: (id) => toggleTodo(set, get, id),
+      setAllowRepeatedDescriptions: (value: boolean) =>
+        utils.setAllowRepeatedDescriptions(set, value),
+      // Funciones de utilidad
+      getTodos: (filter: Filter) => utils.getTodos(get, filter),
+      getCompletionResume: () => utils.getCompletionResume(get),
+      addTodo: (todo) => utils.addTodo(set, get, todo),
+      removeTodo: (id) => utils.removeTodo(set, get, id),
+      clearTodos: () => utils.clearTodos(set, get),
+      editTodo: (id, description) => utils.editTodo(set, get, id, description),
+      toggleTodo: (id) => utils.toggleTodo(set, get, id),
     }),
     {
       name: 'todo-list',
--- src/store/todoFunctions.ts
+++ src/store/todoFunctions.ts
@@ -9,10 +9,12 @@ import {
   ToggleTodo,
 } from './types';
 
+// Esta función es la que se encarga de cambiar el valor de la variable de estado
 export const setAllowRepeatedDescriptions: SetAllowRepeatedDescriptions = (set, value) => {
   set({ allowRepeatedDescriptions: value });
 };
 
+// Esta función es la que se encarga de filtrar la lista de tareas
 export const getTodos: GetTodos = (get, filter) => {
   const state = get();
   if (filter === 'all') return state.todos;
@@ -21,6 +23,7 @@ export const getTodos: GetTodos = (get, filter) => {
   return state.todos;
 };
 
+// Esta función es la que se encarga de calcular el total de tareas completadas
 export const getCompletionResume: GetCompletionResume = (get) => {
   const state = get();
   const totalTodos = state.todos.length;
@@ -28,6 +31,8 @@ export const getCompletionResume: GetCompletionResume = (get) => {
   return `${completedTodos}/${totalTodos}`;
 };
 
+// Esta función es la que se encarga de agregar una tarea a la lista
+// Si la tarea ya existe y no se permite repetir descripciones, se devuelve un error
 export const addTodo: AddTodo = (set, get, todo) => {
   const state = get();
   const todoExists = state.todos.find(
@@ -40,13 +45,17 @@ export const addTodo: AddTodo = (set, get, todo) => {
   set({ todos: [...state.todos, todo] });
 };
 
+// Esta función es la que se encarga de eliminar una tarea de la lista
 export const removeTodo: RemoveTodo = (set, get, id) => {
   const state = get();
   set({ todos: state.todos.filter((todo) => todo.id !== id) });
 };
 
+// Esta función es la que se encarga de eliminar todas las tareas de la lista
 export const clearTodos: ClearTodos = (set) => set({ todos: [] });
 
+// Esta función es la que se encarga de editar una tarea de la lista
+// Si la tarea ya existe y no se permite repetir descripciones, se devuelve un error
 export const editTodo: EditTodo = (set, get, id, description) => {
   const state = get();
   const todoExists = state.todos.find(
@@ -58,6 +67,7 @@ export const editTodo: EditTodo = (set, get, id, description) => {
   set({ todos: state.todos.map((todo) => (todo.id === id ? { ...todo, description } : todo)) });
 };
 
+// Esta función es la que se encarga de cambiar el estado de una tarea de la lista
 export const toggleTodo: ToggleTodo = (set, get, id) => {
   const state = get();
   set({
